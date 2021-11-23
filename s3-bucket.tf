fa5e01658f499cd0a088b157b8ad7debe3213292
Shantanu Panda <shantanu1804@gmail.com>
2021-11-23 14:28:10 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,6 @@
 module "s3_bucket" {
   source  = "app.terraform.io/example-org-efc3b6/s3-bucket/aws"
   version = "2.2.0"
-
-  bucket = "my-s3-bucket"
   acl    = "private"
   bucket_prefix = var.prefix
   versioning = {
