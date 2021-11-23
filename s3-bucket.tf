b8c62f77ab7e7793fc2bc7a30186211a02f0ed99
Shantanu <shantanu1804@gmail.com>
2021-11-23 20:11:31 +0530

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/example-org-efc3b6/s3-bucket/aws"
   version = "2.2.0"
   acl    = "private"
-  bucket_prefix = var.prefix
+  bucket_prefix = "test"
   versioning = {
     enabled = true
   }
